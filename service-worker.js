9c12262ebb6c74b3c5c463a59b3c0c30092aa9f0
3ooowojtooo <3ooowojtooo@users.noreply.github.com>
2023-06-24 15:50:08 +0000

    deploy: a710cc403ae8638d8d5ff8b98bfd2e2671e664f6

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55f909d947ce674071e8c829fd9970ae.js"
+  "/email-sender-gh-pages/precache-manifest.6348a1e45266ee878950b956671dc48a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/email-sender-gh-pages/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
